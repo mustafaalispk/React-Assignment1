d9927c4d0b54fbbf0bd91ea5f783d8f7dff336da
mustafaalispk <54665140+mustafaalispk@users.noreply.github.com>
2020-12-07 16:41:19 +0100

    installed axios

--- reactassignment1/src/views/HomeView.js
+++ reactassignment1/src/views/HomeView.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
 
 const HomeView = () => {
+    const [num, setNum] = useState();
     return (
-        <div className="contact_form">
-            <h1>This is the Homeview page!</h1>
-        </div>
+        <>
+            <h1>you choose {num} value</h1>
+            <select value={num} onChange={(event) => {
+                setNum(event.target.value);
+            }}>
+                <option value="1">1</option>
+                <option value="25">25</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="6">6</option>
+            </select>
+        </>
     )
 }
 export default HomeView;
