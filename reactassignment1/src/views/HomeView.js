6d43c21357f66cf15cd9969dfe61b34a1318df34
mustafaalispk <54665140+mustafaalispk@users.noreply.github.com>
2020-12-08 15:28:19 +0100

    getting pokemon data name moves

--- reactassignment1/src/views/HomeView.js
+++ reactassignment1/src/views/HomeView.js
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 const HomeView = () => {
     const [num, setNum] = useState();
+    const [name, setName] = useState();
+    const [moves, setMoves] = useState();
+    const [, setLoading] = useState(true)
+    useEffect(() => {
+        const getData = async () => {
+            setLoading(true)
+            try {
+                const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
+                console.log(res.data);
+                setName(res.data.name);
+                setMoves(res.data.moves[0].move.name);
+            }
+            catch (error) {
+                console.log(error)
+                setLoading(false)
+            }
+        }
+        getData();
+    });
     return (
         <>
-            <h1>you choose {num} value</h1>
+            <h1>you choose <span style={{ color: 'red' }}>{num} value </span></h1>
+            <h1>you choose <span style={{ color: 'red' }}> {name} </span></h1>
+            <h1>you choose <span style={{ color: 'red' }}> {moves} moves </span></h1>
             <select value={num} onChange={(event) => {
                 setNum(event.target.value);
             }}>
